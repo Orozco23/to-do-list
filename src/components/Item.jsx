cfc3503a16694edd545bf5426eafe8a62f1c5c3a
Marisol López <jennymlo2399@gmail.com>
2025-03-06 01:18:02 -0600

    feat: add item component

---
+++ src/components/Item.jsx
@@ -0,0 +1,15 @@
+export default function Item({ color, status, text }) {
+    const icons = ["bi bi-check2-square icon-medium", "bi bi-square icon-small-square"]
+    const classes = ['item strike-out ', 'item ']
+
+    return (
+        <li className={classes[status] + color + ' to-do'}>
+            <i className={icons[status]} />
+            <h4>
+                {text}
+            </h4>
+            <i className="bi bi-pencil icon-small" />
+            <i className="bi bi-trash3 icon-small" />
+        </li>
+    )
+}

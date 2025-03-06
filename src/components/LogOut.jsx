5bc3135c5bcc6ffe5d437f44e4b25d2f56e7659f
Marisol López <jennymlo2399@gmail.com>
2025-03-06 01:20:12 -0600

    feat: add log out component

---
+++ src/components/LogOut.jsx
@@ -0,0 +1,8 @@
+export default function LogOut() {
+    return (
+        <div className="grid primary-color">
+            <h2>TO DO LIST</h2>
+            <i className="bi bi-door-closed icon-medium" />
+        </div>
+    )
+}

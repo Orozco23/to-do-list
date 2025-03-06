6fdf1154ccd555d51871c265ad252a95e4917589
Marisol López <jennymlo2399@gmail.com>
2025-03-06 14:40:52 -0600

    feat: update button component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,12 @@
-export default function Button({ text, color }) {
+export default function Button({ type, text, color, event, name, id }) {
     return (
-        <button type="submit" className={color}>
+        <button 
+            type={type} 
+            className={color}
+            onClick={event}
+            name={name}
+            id={id}
+        >
             {text}
         </button>
     )

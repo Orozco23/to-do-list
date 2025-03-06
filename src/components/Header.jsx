92c4de4f814bb66434e39fe87e5a2c9ebb1e1ec4
Marisol López <jennymlo2399@gmail.com>
2025-03-06 01:19:02 -0600

    feat: add header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import Filter from "./Filter";
+
+export default function Header() {
+    return (
+        <header className="secondary-color">
+            <i className="bi bi-list-task icon-large" />
+            <h1 className="bebas-neue-regular">
+                List Of TO DO
+            </h1>
+            <Filter />                
+        </header>
+    )
+}

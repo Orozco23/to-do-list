20c26780b7b09f7690b75d84dae159546277d2da
Marisol López <jennymlo2399@gmail.com>
2025-03-06 14:48:09 -0600

    feat: add open modal on plus circle icon

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,25 @@
 import { Tooltip } from 'react-tooltip'
+import { useState } from "react";
+import ModalCreate from './ModalCreate';
 
 export default function Footer() {
+    const [isOpen, setIsOpen] = useState(false);
+    
+    const toggleModal = () => setIsOpen(!isOpen)
+
     return (
         <footer>               
             <i 
                 data-tooltip-id="create"
                 data-tooltip-content="Add a task"
                 className="bi bi-plus-circle-fill create icon-larger secondary-color" 
+                onClick={toggleModal}
             />
             <Tooltip id="create" />
+            <ModalCreate
+                isOpen={isOpen}
+                toggleModal={toggleModal}
+            />
         </footer>
     )
 }

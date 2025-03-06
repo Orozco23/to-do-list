2f057fd46cd661978ba70ce9fca8eb024d61286b
Marisol López <jennymlo2399@gmail.com>
2025-03-06 01:21:47 -0600

    feat: add filter component

---
+++ src/components/Filter.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react"
+
+export default function Filter() {
+    const options = [
+        {icon: <i className="bi bi-trash2 icon-medium primary-color"/>, value: 'created_at'},
+        {icon: <i className="bi bi-pencil icon-medium"/>, value: 'title'},
+        {icon: <i className="bi bi-trash3 icon-medium primary-color"/>, value: 'is_completed'}
+    ]
+    const [isOpen, setIsOpen] = useState(false)
+    const [selectedOption, setSelectedOption] = useState('All')
+
+    const toggleDropdown = () => setIsOpen(!isOpen)
+
+    const handleSelect = (option) => {
+        setSelectedOption(option)
+        setIsOpen(false)
+    }
+
+    return (
+        <div className="div-filter">
+            <div onClick={toggleDropdown}>
+                <span role="icon" label="filter"><i className="bi bi-funnel secondary-color icon-large"/></span>
+            </div>
+
+            {isOpen && (
+                <ul className="filter-selector">
+                    {
+                        options.map((option, index) =>
+                            <li key={index} onClick={() => handleSelect(option.value)} name={option.value}>{option.icon}</li>
+                        )
+                    }
+                </ul>
+            )}
+        </div>
+    )
+}

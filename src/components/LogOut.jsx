17bdf01b478c4096d4832e2090588e5e74f7befd
Marisol López <jennymlo2399@gmail.com>
2025-03-07 02:21:54 -0600

    feat: functional logout

--- src/components/LogOut.jsx
+++ src/components/LogOut.jsx
@@ -1,6 +1,17 @@
 import { Tooltip } from "react-tooltip";
+import useStore from "../store/useStore";
+import { useNavigate } from "react-router-dom";
 
 export default function LogOut() {
+    
+    const { removeAll } = useStore()
+    const navigate = useNavigate()
+
+    const removeStates = () => {
+        removeAll()
+        navigate('/')
+    }
+
     return (
         <div className="grid primary-color">
             <h2>TO DO LIST</h2>
@@ -8,6 +19,7 @@ export default function LogOut() {
                 className="bi bi-door-closed icon-medium"
                 data-tooltip-id="logout"
                 data-tooltip-content="Log Out"
+                onClick={removeStates}
             />
             <Tooltip id="logout" />
         </div>
